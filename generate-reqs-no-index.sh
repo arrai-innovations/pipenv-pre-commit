5e57e8e09a466fb72a0add14994bef689ef706f8
Joel Hillacre <joel@403forbidden.ca>
2022-10-28 14:05:17 -0600

    feat: remove extra indexes and other dash flags.
    This allows the generated file to be PEP 508 compatible, for use with
    setuptools.

--- generate-reqs-no-index.sh
+++ generate-reqs-no-index.sh
@@ -4,4 +4,4 @@ if  ! (pipenv requirements > requirements.txt); then
     echo 'Trying legacy "pipenv lock -r" function.'
     pipenv lock -r > requirements.txt
 fi
-sed -i '' -e '/^-i/d' requirements.txt
+sed -i -e '/^-/d' requirements.txt
